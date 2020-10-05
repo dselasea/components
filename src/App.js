e02c51b79b81d856daf0b03c8376d6a7e256aa57
Daniel Selase Anyidoho <dselasea@gmail.com>
2020-10-05 18:00:05 +0000

    Third Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Body from "./Body";
+import CreateReactApp from "./CreateReactApp";
 
 function App() {
   return (
     <div>
       <Header />
       <Body />
+      <CreateReactApp />
     </div>
   );
 }
---
+++ src/CreateReactApp.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function CreateReactApp() {
+  return (
+    <div>
+      <h2>Create a New React App</h2>
+      <h4>
+        Use an integrated toolchain for the best user and developer experience.
+      </h4>
+      <p>
+        This page describes a few popular React toolchains which help with tasks
+        like:
+      </p>
+    </div>
+  );
+}
+
+export default CreateReactApp;
