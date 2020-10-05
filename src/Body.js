495cda7862e50140d6f76bafb0eaa0fbad907399
Daniel Selase Anyidoho <dselasea@gmail.com>
2020-10-05 17:46:13 +0000

    Adding First Component

---
+++ src/Body.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Body() {
+  return (
+    <div>
+      <h2>What are components?</h2>
+      <p>
+        Components let you split the UI into independent, reusable pieces, and
+        think about each piece in isolation. This page provides an introduction
+        to the idea of components.
+      </p>
+    </div>
+  );
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <h1>React Components</h1>
+      <p>Practicing how to use react Components</p>
+    </div>
+  );
+}
+
+export default Header;
