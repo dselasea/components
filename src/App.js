4b149b283d1c184a4fe40bc2176949b8f360b9ad
Daniel Selase Anyidoho <dselasea@gmail.com>
2020-10-05 17:53:35 +0000

    Adding Second Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Body from "./Body";
 
 function App() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
